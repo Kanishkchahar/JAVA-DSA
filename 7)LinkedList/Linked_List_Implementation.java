46114284363afe837511a77de52915e0c94703c4
Kanishkchahar <kanishkchahar34@gmail.com>
2026-04-05 08:45:38 +0530

    added linked list

---
+++ 7)LinkedList/Linked_List_Implementation.java
@@ -0,0 +1,16 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int new_data) {
+        this.data = new_data;
+        this.next = null;
+    }
+}
+
+public class Linked_List_Implementation {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ 7)LinkedList/basic.java
@@ -0,0 +1,29 @@
+public class basic {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(6);
+        Node c = new Node(7);
+        Node d = new Node(8);
+        Node e = new Node(9);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(a.data);
+        System.out.println(a.next.data);
+        System.out.println(a.next.next.data);
+        System.out.println(a.next.next.next.data);
+        System.out.println(a.next.next.next.next.data);
+
+
+    }
+}
