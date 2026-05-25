e360637efa21be7378e8ba1ca203abf3dcd0aaea
Kanishkchahar <kanishkchahar34@gmail.com>
2026-05-25 11:43:55 +0530

    add sorting

---
+++ Bubble sort/Bubble.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Bubble {
+    public static void main(String[] args) {
+    int[] arr = {5,4,3,2,1};
+    bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubbleSort(int[] arr){
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length-i; j++) {
+
+                if(arr[j-1] > arr[j]){
+
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+
+    }
+
+}
